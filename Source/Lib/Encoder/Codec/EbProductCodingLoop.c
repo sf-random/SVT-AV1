7fe900852c792d692cd0ad8982f2219d69d43a4e
Jing Li <jing.b.li@intel.com>
2020-04-08 09:29:25 +0800

    clean up names of qp to qindex
    Signed-off-by: Jing Li <jing.b.li@intel.com>

--- Source/Lib/Encoder/Codec/EbProductCodingLoop.c
+++ Source/Lib/Encoder/Codec/EbProductCodingLoop.c
@@ -4849,8 +4849,8 @@ void av1_cost_calc_cfl(PictureControlSet *pcs_ptr, ModeDecisionCandidateBuffer *
     // FullLoop and TU search
     int32_t  alpha_q3;
 #if QP2QINDEX
-    uint16_t cb_qp = context_ptr->qp_index;
-    uint16_t cr_qp = context_ptr->qp_index;
+    uint16_t cb_qindex = context_ptr->qp_index;
+    uint16_t cr_qindex = context_ptr->qp_index;
 #else
     uint16_t cb_qp = context_ptr->qp;
     uint16_t cr_qp = context_ptr->qp;
@@ -4920,8 +4920,13 @@ void av1_cost_calc_cfl(PictureControlSet *pcs_ptr, ModeDecisionCandidateBuffer *
                     input_picture_ptr,
                     pcs_ptr,
                     PICTURE_BUFFER_DESC_Cb_FLAG,
+#if QP2QINDEX
+                    cb_qindex,
+                    cr_qindex,
+#else
                     cb_qp,
                     cr_qp,
+#endif
                     &(*count_non_zero_coeffs[1]),
                     &(*count_non_zero_coeffs[2]));
 
@@ -5004,8 +5009,13 @@ void av1_cost_calc_cfl(PictureControlSet *pcs_ptr, ModeDecisionCandidateBuffer *
                     input_picture_ptr,
                     pcs_ptr,
                     PICTURE_BUFFER_DESC_Cr_FLAG,
+#if QP2QINDEX
+                    cb_qindex,
+                    cr_qindex,
+#else
                     cb_qp,
                     cr_qp,
+#endif
                     &(*count_non_zero_coeffs[1]),
                     &(*count_non_zero_coeffs[2]));
         candidate_ptr->v_has_coeff = *count_non_zero_coeffs[2] ? EB_TRUE : EB_FALSE;
@@ -5588,8 +5598,13 @@ static INLINE TxType av1_get_tx_type(BlockSize sb_type, int32_t is_inter, Predic
 void check_best_indepedant_cfl(PictureControlSet *pcs_ptr, EbPictureBufferDesc *input_picture_ptr,
                                ModeDecisionContext *context_ptr, uint32_t input_cb_origin_in_index,
                                uint32_t                     blk_chroma_origin_index,
-                               ModeDecisionCandidateBuffer *candidate_buffer, uint8_t cb_qp,
-                               uint8_t cr_qp, uint64_t *cb_full_distortion,
+                               ModeDecisionCandidateBuffer *candidate_buffer,
+#if QP2QINDEX
+                               uint8_t cb_qindex, uint8_t cr_qindex,
+#else
+                               uint8_t cb_qp, uint8_t cr_qp,
+#endif
+                               uint64_t *cb_full_distortion,
                                uint64_t *cr_full_distortion, uint64_t *cb_coeff_bits,
                                uint64_t *cr_coeff_bits) {
     uint32_t full_lambda =  context_ptr->hbd_mode_decision ?
@@ -5723,8 +5738,13 @@ void check_best_indepedant_cfl(PictureControlSet *pcs_ptr, EbPictureBufferDesc *
                     input_picture_ptr,
                     pcs_ptr,
                     PICTURE_BUFFER_DESC_CHROMA_MASK,
+#if QP2QINDEX
+                    cb_qindex,
+                    cr_qindex,
+#else
                     cb_qp,
                     cr_qp,
+#endif
                     &(*count_non_zero_coeffs[1]),
                     &(*count_non_zero_coeffs[2]));
 
@@ -6170,7 +6190,11 @@ void tx_reset_neighbor_arrays(PictureControlSet *pcs_ptr, ModeDecisionContext *c
 
 void tx_type_search(PictureControlSet *pcs_ptr,
                     ModeDecisionContext *context_ptr, ModeDecisionCandidateBuffer *candidate_buffer,
+#if QP2QINDEX
+                    uint32_t qindex) {
+#else
                     uint32_t qp) {
+#endif
     EbPictureBufferDesc *input_picture_ptr = context_ptr->hbd_mode_decision
                                                  ? pcs_ptr->input_frame16bit
                                                  : pcs_ptr->parent_pcs_ptr->enhanced_picture_ptr;
@@ -6297,7 +6321,11 @@ void tx_type_search(PictureControlSet *pcs_ptr,
             &(((int32_t *)candidate_buffer->residual_quant_coeff_ptr
                    ->buffer_y)[context_ptr->txb_1d_offset]),
             &(((int32_t *)candidate_buffer->recon_coeff_ptr->buffer_y)[context_ptr->txb_1d_offset]),
+#if QP2QINDEX
+            qindex,
+#else
             qp,
+#endif
             seg_qp,
             context_ptr->blk_geom->tx_width[context_ptr->tx_depth][context_ptr->txb_itr],
             context_ptr->blk_geom->tx_height[context_ptr->tx_depth][context_ptr->txb_itr],
@@ -6978,7 +7006,11 @@ void update_tx_candidate_buffer(ModeDecisionCandidateBuffer *candidate_buffer,
 void perform_tx_partitioning(ModeDecisionCandidateBuffer *candidate_buffer,
                              ModeDecisionContext *context_ptr, PictureControlSet *pcs_ptr,
                              uint64_t ref_fast_cost, uint8_t start_tx_depth, uint8_t end_tx_depth,
+#if QP2QINDEX
+                             uint32_t qindex, uint32_t *y_count_non_zero_coeffs, uint64_t *y_coeff_bits,
+#else
                              uint32_t qp, uint32_t *y_count_non_zero_coeffs, uint64_t *y_coeff_bits,
+#endif
                              uint64_t *y_full_distortion) {
     uint32_t full_lambda =  context_ptr->hbd_mode_decision ?
         context_ptr->full_lambda_md[EB_10_BIT_MD] :
@@ -7088,7 +7120,11 @@ void perform_tx_partitioning(ModeDecisionCandidateBuffer *candidate_buffer,
                     32 &&
                 context_ptr->blk_geom->tx_height[context_ptr->tx_depth][context_ptr->txb_itr] <= 32)
                 if (!tx_search_skip_flag) {
+#if QP2QINDEX
+                    tx_type_search(pcs_ptr, context_ptr, tx_candidate_buffer, qindex);
+#else
                     tx_type_search(pcs_ptr, context_ptr, tx_candidate_buffer, qp);
+#endif
                 }
 
             product_full_loop(tx_candidate_buffer,
@@ -7279,8 +7315,8 @@ void full_loop_core(PictureControlSet *pcs_ptr, SuperBlock *sb_ptr, BlkStruct *b
 
     // FullLoop and TU search
 #if QP2QINDEX
-    uint16_t cb_qp = context_ptr->qp_index;
-    uint16_t cr_qp = context_ptr->qp_index;
+    uint16_t cb_qindex = context_ptr->qp_index;
+    uint16_t cr_qindex = context_ptr->qp_index;
 #else
     uint16_t cb_qp = context_ptr->qp;
     uint16_t cr_qp = context_ptr->qp;
@@ -7338,8 +7374,13 @@ void full_loop_core(PictureControlSet *pcs_ptr, SuperBlock *sb_ptr, BlkStruct *b
                         input_picture_ptr,
                         pcs_ptr,
                         PICTURE_BUFFER_DESC_CHROMA_MASK,
+#if QP2QINDEX
+                        cb_qindex,
+                        cr_qindex,
+#else
                         cb_qp,
                         cr_qp,
+#endif
                         &(*count_non_zero_coeffs[1]),
                         &(*count_non_zero_coeffs[2]));
 
@@ -7369,8 +7410,13 @@ void full_loop_core(PictureControlSet *pcs_ptr, SuperBlock *sb_ptr, BlkStruct *b
                                               input_cb_origin_in_index,
                                               blk_chroma_origin_index,
                                               candidate_buffer,
+#if QP2QINDEX
+                                              (uint8_t)cb_qindex,
+                                              (uint8_t)cr_qindex,
+#else
                                               (uint8_t)cb_qp,
                                               (uint8_t)cr_qp,
+#endif
                                               cb_full_distortion,
                                               cr_full_distortion,
                                               &cb_coeff_bits,
@@ -8384,8 +8430,8 @@ void search_best_independent_uv_mode(PictureControlSet *  pcs_ptr,
         candidate_buffer->candidate_ptr =
             &context_ptr->fast_candidate_array[uv_cand_buff_indices[uv_mode_count]- start_full_buffer_index + start_fast_buffer_index];
 #if QP2QINDEX
-        uint16_t cb_qp                               = context_ptr->qp_index;
-        uint16_t cr_qp                               = context_ptr->qp_index;
+        uint16_t cb_qindex                           = context_ptr->qp_index;
+        uint16_t cr_qindex                           = context_ptr->qp_index;
 #else
         uint16_t cb_qp                               = context_ptr->qp;
         uint16_t cr_qp                               = context_ptr->qp;
@@ -8431,8 +8477,13 @@ void search_best_independent_uv_mode(PictureControlSet *  pcs_ptr,
                     input_picture_ptr,
                     pcs_ptr,
                     PICTURE_BUFFER_DESC_CHROMA_MASK,
+#if QP2QINDEX
+                    cb_qindex,
+                    cr_qindex,
+#else
                     cb_qp,
                     cr_qp,
+#endif
                     &(*count_non_zero_coeffs[1]),
                     &(*count_non_zero_coeffs[2]));
 
