a810805ea005d493d28f518ce54ac6c52e0eb4fa
hguermaz <hassen.guermazi@intel.com>
2020-05-20 05:53:17 -0700

    #if 1 // ME for NSQ @ MD

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -428,13 +428,14 @@ extern "C" {
 #define REFACTOR_REF_FRAME_MASKING 0
 #define PRUNING_PER_INTER_TYPE 0
 #define PD0_INTER_CAND 0
-
-#define SHUT_ME_NSQ_SEARCH 0 
+#if 1 // ME for NSQ @ MD
+#define SHUT_ME_NSQ_SEARCH 1 
 #define ADD_MD_NSQ_SEARCH 1
 #define REDESIGN_ME_RESULTS_MD 1
-
+#define NSQ_REMOVAL_CODE_CLEAN_UP 1 // should be lossless
+#endif
 #define REMOVE_ME_BIPRED_SEARCH 0
-#define NSQ_REMOVAL_CODE_CLEAN_UP 0 // should be lossless
+
 /********************************/
 
 #endif
