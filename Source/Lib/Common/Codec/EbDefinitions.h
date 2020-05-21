3704608a0ffbbcca453e1b1b66bd1bec0532350e
hguermaz <hassen.guermazi@intel.com>
2020-05-20 20:14:11 -0700

    clean 1

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -414,22 +414,19 @@ extern "C" {
 #define MAY19_ADOPTIONS             1 // Adoptions in MR, M5-M8 from svt-01_presets branch
 
 /********************************/
-#define SHUT_ME_CAND_SORTING 1 
+#define SHUT_ME_CAND_SORTING 0 
 #define REFACTOR_REF_FRAME_MASKING 0
 #define PRUNING_PER_INTER_TYPE 0
 #define PD0_INTER_CAND 0
 #if 1 // ME for NSQ @ MD
-
 /////*** SHUT ME NSQ @ ME ***
 #define SHUT_ME_NSQ_SEARCH 1 
 #define FIX_SHUT_ME_NSQ_SEARCH 1 
 /////*** SHUT ME NSQ @ ME ***
-
 #define ADD_MD_NSQ_SEARCH 1
 #define REDESIGN_ME_RESULTS_MD 0
 #define NSQ_REMOVAL_CODE_CLEAN_UP 0 // should be lossless
-define REMOVE_ME_BIPRED_SEARCH 0
-
+#define REMOVE_ME_BIPRED_SEARCH 0
 /********************************/
 #endif
 #endif
