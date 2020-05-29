6e44ab3be35d02f8717a3373a3c0b17f07b84a07
hguermaz <hassen.guermazi@intel.com>
2020-05-28 23:45:36 -0700

    do_4x4 = 0

--- Source/Lib/Encoder/Codec/EbEncDecProcess.c
+++ Source/Lib/Encoder/Codec/EbEncDecProcess.c
@@ -1759,7 +1759,7 @@ void md_subpel_search_controls(ModeDecisionContext *mdctxt, uint8_t md_subpel_se
         md_subpel_search_ctrls->enabled = 1;
         md_subpel_search_ctrls->use_ssd = 0;
 
-        md_subpel_search_ctrls->do_4x4 = 1;
+        md_subpel_search_ctrls->do_4x4 = 0;
         md_subpel_search_ctrls->do_nsq = 1;
 
         md_subpel_search_ctrls->half_pel_search_enabled          = 1;
@@ -1788,8 +1788,7 @@ void md_subpel_search_controls(ModeDecisionContext *mdctxt, uint8_t md_subpel_se
         md_subpel_search_ctrls->enabled = 1;
         md_subpel_search_ctrls->use_ssd = 0;
 
-
-        md_subpel_search_ctrls->do_4x4 = 1;
+        md_subpel_search_ctrls->do_4x4 = 0;
         md_subpel_search_ctrls->do_nsq = 1;
 
         md_subpel_search_ctrls->half_pel_search_enabled          = 1;
@@ -1820,7 +1819,7 @@ void md_subpel_search_controls(ModeDecisionContext *mdctxt, uint8_t md_subpel_se
 
 
         md_subpel_search_ctrls->do_4x4 = 0;
-        md_subpel_search_ctrls->do_nsq = 0;
+        md_subpel_search_ctrls->do_nsq = 1;
 
         md_subpel_search_ctrls->half_pel_search_enabled = 1;
         md_subpel_search_ctrls->half_pel_search_scan    = 1;
