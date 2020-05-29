6cab07f9d816a634f68516044c42fd47142fb5bb
hguermaz <hassen.guermazi@intel.com>
2020-05-29 16:05:52 -0700

    back to ref

--- Source/Lib/Encoder/Codec/EbEncDecProcess.c
+++ Source/Lib/Encoder/Codec/EbEncDecProcess.c
@@ -1916,7 +1916,7 @@ void md_subpel_search_controls(ModeDecisionContext *mdctxt, uint8_t md_subpel_se
 
 
         md_subpel_search_ctrls->do_4x4 = 1;
-        md_subpel_search_ctrls->do_nsq = 0;
+        md_subpel_search_ctrls->do_nsq = 1;
 
         md_subpel_search_ctrls->half_pel_search_enabled = 1;
         md_subpel_search_ctrls->half_pel_search_scan    = 1;
