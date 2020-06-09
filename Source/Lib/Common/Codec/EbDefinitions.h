2a9e4fcf081439eb66869aa85e2e5e15684b01e6
hguermaz <hassen.guermazi@intel.com>
2020-06-09 09:49:39 -0700

    shut SQ_WEIGHT_110

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -492,7 +492,7 @@ extern "C" {
 #define MR_TEST_7 0 // chroma
 #define MR_TEST_8 0 // new_nearest_near_comb_injection
 
-#define SQ_WEIGHT_110                  1
+#define SQ_WEIGHT_110                  0
 #define PRUNE_NEAREST_NEW_NEAR_CLOSEST 0
 #define PRUNE_NEAREST_NEW_NEAR_BEST    0
 #define TXS_INTER_DEPTH_1              0
