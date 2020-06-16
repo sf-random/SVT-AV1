5010b1c044e47b65dc2a14d0d13fd7464f96ce83
hguermaz <hassen.guermazi@intel.com>
2020-06-15 17:44:07 -0700

    quarter_pel_search_pos_cnt=2

--- Source/Lib/Encoder/Codec/EbEncDecProcess.c
+++ Source/Lib/Encoder/Codec/EbEncDecProcess.c
@@ -1849,7 +1849,7 @@ void md_subpel_search_controls(ModeDecisionContext *mdctxt, uint8_t md_subpel_se
         md_subpel_search_ctrls->quarter_pel_search_enabled = 1;
         md_subpel_search_ctrls->quarter_pel_search_scan    = 0;
 #if IMPROVE_QUARTER_PEL        
-        md_subpel_search_ctrls->quarter_pel_search_pos_cnt = 5;
+        md_subpel_search_ctrls->quarter_pel_search_pos_cnt = 2;
 #endif
         md_subpel_search_ctrls->eight_pel_search_enabled = 1;
         md_subpel_search_ctrls->eight_pel_search_scan    = 0;
