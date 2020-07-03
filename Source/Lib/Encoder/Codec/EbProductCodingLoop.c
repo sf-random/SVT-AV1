f23c833a5d5e3d3be742706168bf6077a9ce2882
hguermaz <hassen.guermazi@intel.com>
2020-07-02 21:15:13 -0700

    SUPER_HEAVY

--- Source/Lib/Encoder/Codec/EbProductCodingLoop.c
+++ Source/Lib/Encoder/Codec/EbProductCodingLoop.c
@@ -5311,9 +5311,8 @@ void md_sq_motion_search(PictureControlSet *pcs_ptr, ModeDecisionContext *contex
         uint32_t fast_lambda = context_ptr->hbd_mode_decision ?
             context_ptr->fast_lambda_md[EB_10_BIT_MD] :
             context_ptr->fast_lambda_md[EB_8_BIT_MD];
-#if !HEAVY_SEARCH
+
         if (RDCOST(fast_lambda, 16, pa_me_distortion) > RDCOST(fast_lambda, 16, 5 * context_ptr->blk_geom->bwidth * context_ptr->blk_geom->bheight))
-#endif
         {
             {
                 EbReferenceObject *ref_obj = (EbReferenceObject *)pcs_ptr->ref_pic_ptr_array[list_idx][ref_idx]->object_ptr;
